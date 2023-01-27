4ffe2487be3dcc68565943508a17f366bd09e763
Alex O Bunnyshell <eaasmigrations@bunnyshell.com>
2023-01-27 02:27:44 +0200

    add index file to bucket, serve html

--- s3/main.tf
+++ s3/main.tf
@@ -1,8 +1,18 @@
 provider "aws" {}
 
-resource "aws_s3_bucket" "demo_bunnyshell_books" {
+resource "aws_s3_bucket" "bunny-s3-bucket" {
   bucket = var.bucket_name
-  acl = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+   
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "bunny-cors" {
+  bucket = aws_s3_bucket.bunny-s3-bucket.id
 
   cors_rule {
     allowed_headers = ["*"]
@@ -11,4 +21,21 @@ resource "aws_s3_bucket" "demo_bunnyshell_books" {
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
+
+
+}
+
+resource "aws_s3_bucket_acl" "bunny-bucket-acl" {
+  bucket = aws_s3_bucket.bunny-s3-bucket.id
+  acl    = "public-read"
+}
+
+
+resource "aws_s3_bucket_object" "bunny-s3-fil" {
+  key                    = "index.html"
+  bucket                 = aws_s3_bucket.bunny-s3-bucket.id
+  source                 = "index.html"
+  acl = "public-read"
+
+  content_type = "text/html"
 }
--- s3/outputs.tf
+++ s3/outputs.tf
@@ -1,6 +1,15 @@
-output "s3_bucket_name" {
-  value = aws_s3_bucket.demo_bunnyshell_books.id
+output "s3_bucket_id" {
+  value = aws_s3_bucket.bunny-s3-bucket.id
 }
 output "s3_bucket_region" {
-  value = aws_s3_bucket.demo_bunnyshell_books.region
+  value = aws_s3_bucket.bunny-s3-bucket.region
+}
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.bunny-s3-bucket.arn
+}
+
+
+output "s3_bucket_website_endpoint" {
+  value = aws_s3_bucket.bunny-s3-bucket.website_endpoint
 }
